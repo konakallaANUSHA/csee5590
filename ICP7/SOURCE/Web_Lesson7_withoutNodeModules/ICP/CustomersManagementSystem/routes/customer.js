6af1c40efbb349abe152b2a54df92ca9b1adba85
konakallaANUSHA <47069281+konakallaANUSHA@users.noreply.github.com>
2019-03-10 09:05:30 +0530

    -

---
+++ ICP7/SOURCE/Web_Lesson7_withoutNodeModules/ICP/CustomersManagementSystem/routes/customer.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var Customer = require('../models/Customer');
+
+/* Get customers list */
+router.get('/', function (req, res, next) {
+  Customer.find(function (err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+/* Get single customer by ID */
+router.get('/:id', function (req, res, next) {
+  Customer.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* Save customer record */
+router.post('/', function (req, res, next) {
+  Customer.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* UPDATE */
+router.put('/:id', (req, res, next) => {
+  Book.findByIdAndUpdate(req.params.id, req.body, (err, post) => {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE  */
+router.delete('/:id', (req, res, next) => {
+  Book.findByIdAndRemove(req.params.id, req.body, (err, post) => {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
